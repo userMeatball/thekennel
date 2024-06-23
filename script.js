9da65412f1fca1fd14691285cd765d10fa833cf9
userMeatball <52832724+userMeatball@users.noreply.github.com>
2024-06-23 13:57:05 +0100

    fix desktop, introduce mobile viewport

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const dogForm = document.getElementById("dogForm");
-const dogName = document.getElementById("dogName");
-
 dogForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    console.log(dogName.value);
+    let dogForm = document.querySelector("#dogForm");
+    let dogName = document.querySelector("#dogName");
+    let dogGender = document.querySelector("#dogGender");
+    let dogAge = document.querySelector("#dogAge");
+    let dogHealth = document.querySelector("#dogHealth");
+    let dogFriendly = document.querySelector("#dogFriendly");
+    let dogExtra = document.querySelector("#dogExtra");
 })
--- todo.md
+++ todo.md
@@ -1,5 +1,13 @@
 learn md
 git
+vue, react, node etc, tailwind, .scss
+standards
+privacy
 
-replace padding margins if needed
-viewport size
+
+focus on viewport mobile
+
+go through it again obvs
+want to load mobile only on mobile ya get me??
+tablet viewport
+script - creating divs based on viewport
